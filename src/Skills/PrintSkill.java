0518902654433123a6885ba011bb2bebead969a5
Zain Farhah <Zeinfarha123@gmail.com>
2021-02-03 20:26:01 +0100

    added print skill that is incomplete

---
+++ src/Skills/PrintSkill.java
@@ -0,0 +1,31 @@
+package Skills;
+
+import backend.Result;
+import backend.SkillDispatcher;
+import backend.Skill;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+
+public class PrintSkill extends SkillDispatcher {
+    private String keyword = "print";
+
+    protected PrintSkill(String name) {
+        super(name);
+    }
+
+    @Override
+    public double weight(List<String> tokens) {
+        int weight = 0;
+        if (tokens.get(0).toLowerCase().equals(keyword)){
+            weight = 1;
+            return weight;
+        }
+        return weight;
+    }
+
+    @Override
+    public Skill createTask(List<String> tokens, BlockingQueue<Result> resultsQueue) {
+        return null;
+    }
+}
