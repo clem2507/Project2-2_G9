4a423822f45584156f296d0010eda3d6bfa3cc6c
DQBO1998 <dqbo1998@gmail.com>
2021-02-04 17:24:29 +0100

    Modified PrintSkill a little bit to show how to define a SkillDispatcher properly. Keep in mind that the 'weight' method will change in the future.

--- src/main/java/Skills/PrintSkill.java
+++ src/main/java/Skills/PrintSkill.java
@@ -8,32 +8,27 @@ import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
 public class PrintSkill extends SkillDispatcher {
-    private String keyword = "print";
 
-    public PrintSkill(String name) {
-        super(name);
+    public PrintSkill() {
+        super("PrintSomething");
     }
 
     @Override
     public double weight(List<String> tokens) {
-        int weight = 0;
-        if (tokens.get(0).toLowerCase().equals(keyword)){
-            weight = 1;
-            return weight;
-        }
-        return weight;
+        return tokens.get(0).toLowerCase().equals("print")? 1.0:0.0;
     }
 
     @Override
     public Skill createTask(List<String> tokens, BlockingQueue<Result> resultsQueue) {
-        Skill skill = new Skill(new PrintSkill(getUniqueName()),tokens, resultsQueue) {
+        return new Skill(this, tokens, resultsQueue) {
             @Override
             public void run() {
-                for (int x = 1; x<tokens.size(); x++){
-                    System.out.println(tokens.get(x) + " ");
+                String output = "";
+                for (int x = 1; x < tokens.size(); x++){
+                    output += tokens.get(x) + " ";
                 }
+                pushMessage(output);
             }
         };
-        return skill;
     }
 }
