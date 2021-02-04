95f5fa17a06b8e325e700f72400daad852cda282
Zain Farhah <Zeinfarha123@gmail.com>
2021-02-04 20:48:46 +0100

    Added PatternMatcher.java class

---
+++ src/main/java/nlp/PatternMatcher.java
@@ -0,0 +1,19 @@
+package nlp;
+
+import java.util.List;
+
+public class PatternMatcher {
+
+    public static double patternMatcher(List<String> text){
+
+        double match = 0;
+
+
+
+
+
+
+        return match;
+    }
+
+}
