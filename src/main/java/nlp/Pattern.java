464d3dc33e702344ff3a14ca4e0e4553f82d2bbb
DQBO1998 <dqbo1998@gmail.com>
2021-02-05 02:54:23 +0100

    Added methods to the class Pattern to check if a string is a valid integer, day of the week, or date.

--- src/main/java/nlp/Pattern.java
+++ src/main/java/nlp/Pattern.java
@@ -86,11 +86,11 @@ public class Pattern {
                 "friday",
                 "saturday",
                 "sunday"
-        }).contains(str);
+        }).contains(str.toLowerCase());
     }
 
     public static boolean isValidDate(String str){
-        return false;
+        return str.matches("^((?:19|20)[0-9][0-9])-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$");
     }
 
 }
