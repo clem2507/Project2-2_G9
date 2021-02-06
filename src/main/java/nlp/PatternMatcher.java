b67f8a7408303cd36fb989bd5925a94a1a44fd72
joepm95 <jpamuijrers@gmail.com>
2021-02-06 10:55:50 +0100

    Small changes

--- src/main/java/nlp/PatternMatcher.java
+++ src/main/java/nlp/PatternMatcher.java
@@ -7,7 +7,7 @@ import java.util.stream.Stream;
 public class PatternMatcher {
 
     // Method that matches parts of the query with parts of the pattern
-    // Returns list of strings where first part is the part of the pattern and the second is the part of the query that matches it
+    // Returns map where each pair consists of the pattern slot and the tokens from the query that match it
     public static Map<Set<String>, List<String>> patternMatch(String pattern, String query) {
         Map<Set<String>, List<String>> matchList = new HashMap<Set<String>, List<String>>();
         List<Set<String>> patterns = Pattern.parse(pattern);
@@ -17,6 +17,7 @@ public class PatternMatcher {
         Pattern.ContentType currentType;
         for (Set p : patterns) {
             for (Object s : p) {
+                // Since strings will most likely have multiple strings, worked with this
                 if (Pattern.getContentType((String) s).equals(Pattern.ContentType.STRING)) {
                     if (query.contains((String) s)) {
                         matchedString = Tokenizer.asTokenList((String) s);
@@ -28,6 +29,7 @@ public class PatternMatcher {
                 else {
                     List<String> tokens = Tokenizer.asTokenList(query);
                     // TODO: should remove token from query
+                    // Also doesn't work with multi-string parameters
                     for (String token : tokens) {
                         if(Pattern.match((String) s, token)) {
                             matchedString.add(token);
@@ -36,6 +38,8 @@ public class PatternMatcher {
                     }
                 }
                 // TODO: 'empty' patterns (<...> or <#:3>)
+                // TODO: if pattern is matched, move on to next pattern
+                // (e.g. one pattern might be <in, at>. In gets matched, shouldn't look for "at" later in the sentence)
             }
             matchList.put(p, matchedString);
             matchedString = null;
@@ -46,6 +50,7 @@ public class PatternMatcher {
     }
 
     public static void main(String[] args) {
+        //
         Map<Set<String>, List<String>> strings = patternMatch("<test, word><this thing>", "test test this thing");
         System.out.println(strings);
     }
