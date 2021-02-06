69a728dc211232e7534d24c93011331f876434a3
Zain Farhah <Zeinfarha123@gmail.com>
2021-02-06 16:10:23 +0100

    Pattern matching with order into consideration

--- src/main/java/nlp/PatternMatcher.java
+++ src/main/java/nlp/PatternMatcher.java
@@ -11,6 +11,7 @@ public class PatternMatcher {
     public static Map<Set<String>, List<String>> patternMatch(String pattern, String query) {
         Map<Set<String>, List<String>> matchList = new HashMap<Set<String>, List<String>>();
         List<Set<String>> patterns = Pattern.parse(pattern);
+        StringBuffer queryBuffer = new StringBuffer(query);
 
         List<String> matchedString = null;
         int strIndex = 0;
@@ -19,15 +20,22 @@ public class PatternMatcher {
             for (Object s : p) {
                 // Since strings will most likely have multiple strings, worked with this
                 if (Pattern.getContentType((String) s).equals(Pattern.ContentType.STRING)) {
-                    if (query.contains((String) s)) {
+                    //get the length of the object
+                    int objLength = ((String) s).length();
+                    System.out.println(s);
+                    //get substring of the query starting from 0 until length of obj
+                    String substring = queryBuffer.substring(0, objLength);
+                    System.out.println(substring);
+                    if(s.equals(substring)){
                         matchedString = Tokenizer.asTokenList((String) s);
-                        strIndex = query.indexOf((String) s);
-                        // Removing matched part from the query
-                        //query = query.substring(strIndex + ((String) s).length(), query.length());
+                        queryBuffer.replace( 0 ,objLength ,"");
+                        String newQ = queryBuffer.toString().trim();
+                        queryBuffer = new StringBuffer(newQ);
+                        break;
                     }
                 }
                 else {
-                    List<String> tokens = Tokenizer.asTokenList(query);
+                    List<String> tokens = Tokenizer.asTokenList(queryBuffer.toString());
                     // TODO: should remove token from query
                     // Also doesn't work with multi-string parameters
                     for (String token : tokens) {
@@ -44,15 +52,20 @@ public class PatternMatcher {
             matchList.put(p, matchedString);
             matchedString = null;
         }
-
-
         return matchList;
     }
 
     public static void main(String[] args) {
-        //
-        Map<Set<String>, List<String>> strings = patternMatch("<test, word><this thing>", "test test this thing");
+        Map<Set<String>, List<String>> strings = patternMatch("<remind me to, remind me> <on ,in>", "in remind me to");
         System.out.println(strings);
     }
 
 }
+
+
+//                    if (query.contains((String) s)) {
+//                        matchedString = Tokenizer.asTokenList((String) s);
+//                        strIndex = query.indexOf((String) s);
+//                        // Removing matched part from the query
+//                        //query = query.substring(strIndex + ((String) s).length(), query.length());
+//                    }
