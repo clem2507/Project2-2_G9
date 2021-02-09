1774030e8d6659b19f6727ba161114d6c1112dbd
DeLnlyMthrLvr <56164751+DeLnlyMthrLvr@users.noreply.github.com>
2021-02-09 22:16:20 +0100

    Screenshot skill now added with the class Screenshot and the mat2image to transform the matrix of the pixels to a buffered image

---
+++ src/main/java/domains/Photo/Screenshot.java
@@ -0,0 +1,40 @@
+package domains.Photo;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
+public class Screenshot extends JFrame {
+
+    /**
+     * Returns a screnshot taken from the webcam at the moment the class is called
+     * @return
+     */
+    public BufferedImage takeSelfie()
+    {
+        VideoCapture videoCapture = new VideoCapture();
+        Mat mat = new Mat();
+        videoCapture.open(0);
+        if(videoCapture.isOpened())
+        {
+            videoCapture.read(mat);
+            Mat2Image mat2Image = new Mat2Image();
+            return mat2Image.getImage(mat);
+        }
+        else return null;
+    }
+
+    public Screenshot() {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel(new ImageIcon(takeSelfie()));
+        panel.add(label);
+        this.add(panel);
+        this.setSize(500,500);
+        this.setVisible(true);
+    }
+
+}
