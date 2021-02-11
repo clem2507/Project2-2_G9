513f1f759013290f0827c9b9406d45d87fce86d5
Zain Farhah <Zeinfarha123@gmail.com>
2021-02-11 23:44:36 +0100

    Added Time.java that finds the current time in different cities.

---
+++ src/main/java/domains/Time/Time.java
@@ -0,0 +1,54 @@
+package domains.Time;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Time {
+    public static String getTime(String continent, String city) {
+        String urlString = "http://worldtimeapi.org/api/timezone/"+continent+"/"+city;
+
+        String time = "";
+
+        try {
+            StringBuilder result = new StringBuilder();
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+
+
+            int ss = result.indexOf("datetime")+22;
+            for (int k = ss ; k <result.length(); k++){
+                if(isNumeric(String.valueOf(result.charAt(k))) || result.charAt(k)==':'){
+                    time= time+ result.charAt(k);
+                }else{
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+
+        }
+        return time;
+    }
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+}
