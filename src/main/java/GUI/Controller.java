8a03319ccca49f22f40081e9df070eceb30145cc
Zain Farhah <Zeinfarha123@gmail.com>
2021-02-18 19:39:00 +0100

    Added event handler to the drag and drop feature (only stores the file path of a txt file).

--- src/main/java/GUI/Controller.java
+++ src/main/java/GUI/Controller.java
@@ -8,6 +8,8 @@ import javafx.scene.input.*;
 import javafx.scene.text.Text;
 import javafx.scene.paint.Color;
 
+import java.io.File;
+
 public class Controller {
 
     private Main window;
@@ -151,65 +153,32 @@ public class Controller {
             }
         });
 
-        // TODO: EDIT DRAG AND DROP EVENT HANDLER
         Text target = window.target;
-
-        target.setOnDragOver(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                event.acceptTransferModes(TransferMode.ANY);
-                System.out.println("onDragOver");
-
-                /* accept it only if it is  not dragged from the same node
-                 * and if it has a string data */
-                if (event.getGestureSource() != target &&
-                        event.getDragboard().hasString()) {
-                    /* allow for both copying and moving, whatever user chooses */
-                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-                }
-                event.consume();
-            }
-        });
-
-        target.setOnDragDropped(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-
-                System.out.println("onDragDropped");
-                /* if there is a string data on dragboard, read it and use it */
-                Dragboard db = event.getDragboard();
-                boolean success = false;
-                if (db.hasString()) {
-                    target.setText(db.getString());
-                    success = true;
-                }
-                /* let the source know whether the string was successfully
-                 * transferred and used */
-                event.setDropCompleted(success);
-                event.consume();
-            }
-        });
-
-        /**
-        target.setOnDragEntered(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                // the drag-and-drop gesture entered the target
-                System.out.println("onDragEntered");
-                // show to the user that it is an actual gesture target
-                if (event.getGestureSource() != target &&
-                        event.getDragboard().hasString()) {
-                    target.setFill(Color.GREEN);
-                }
-
-                event.consume();
-            }
-        });**/
 
         target.setOnDragExited(new EventHandler <DragEvent>() {
             public void handle(DragEvent event) {
-                /* mouse moved away, remove the graphical cues */
-                System.out.println("hi Zein idk what happens in this event handler lmao");
+
+                Dragboard db = event.getDragboard();
+                File filepath = null;
+                if(getExtension(db.getFiles().get(0).toString()).equals("txt")){
+                    filepath = db.getFiles().get(0);
+                    System.out.println(filepath);
+                }else{
+                    System.out.println("This is not a text file");
+                }
                 target.setFill(Color.BLACK);
                 event.consume();
             }
         });
     }
+    private String getExtension(String fileName){
+        String extension = "";
+
+        int i = fileName.lastIndexOf('.');
+        if (i > 0 && i < fileName.length() - 1) //if the name is not empty
+            return fileName.substring(i + 1).toLowerCase();
+
+        return extension;
+    }
+
 }
