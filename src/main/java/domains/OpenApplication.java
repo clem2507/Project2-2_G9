38eff3e1e6bbbf32d890739d52f2551f1c88d704
DQBO1998 <dqbo1998@gmail.com>
2021-02-18 20:05:54 +0100

    OpenApplication is fully functional on windows now - next OS coming next (hopefully we can optimize this for windows without having to link DLLs compiled for .NET)

--- src/main/java/domains/OpenApplication.java
+++ src/main/java/domains/OpenApplication.java
@@ -1,19 +1,23 @@
 package domains;
 
 import backend.*;
-import backend.common.CurrentDesktop;
+import backend.common.FileSystem;
 import nlp.MatchedSequence;
 
-import java.awt.*;
 import java.io.File;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 
 public class OpenApplication extends Domain {
+    private final Set<FileSystem.LinkData> listedApps;
 
     public OpenApplication() {
         super(DomainNames.OpenApp);
         addPattern("<open, run, execute, exec, start> <@program_name, ...>");
+        listedApps = new HashSet<>();
     }
 
     public Skill dispatchSkill(MatchedSequence sequence, BlockingQueue<AssistantMessage> outputChannel) {
@@ -21,28 +25,68 @@ public class OpenApplication extends Domain {
         Optional<String> programName = programNameIndex.map(sequence::getStringAt);
 
         return new Skill(this, outputChannel) {
+
+            private boolean askFurtherSearch(){
+                assert programName.isPresent();
+                return Popup.binaryQuestion(
+                        "No match found for " + programName.get() +
+                                ". Would you like do run a complete system search? (It may take a while)"
+                );
+            }
+
+            private boolean searchApp(){
+                assert programName.isPresent();
+                Optional<FileSystem.LinkData> bestMatch = FileSystem.findClosestMatch(
+                        listedApps,
+                        programName.get(),
+                        FileSystem.DEFAULT_MIN_THRESHOLD
+                );
+
+                if(bestMatch.isPresent()){
+                    FileSystem.runProgramFromLink(bestMatch.get());
+                    pushMessage("Running " + programName.get(), MessageType.STRING);
+                    return true;
+                }
+
+                return false;
+            }
+
             @Override
             public void run() {
 
                 if(programName.isPresent()){
-                    Optional<CurrentDesktop.LinkData> linkData = CurrentDesktop.getMostSimilarLink(
-                            programName.get(),
-                            CurrentDesktop.DEFAULT_MIN_THRESHOLD
-                    );
 
-                    if(linkData.isPresent()){
-                        CurrentDesktop.runProgramFromLink(linkData.get());
-                        pushMessage("Running " + programName.get(), MessageType.STRING);
-                    }
+                    synchronized (listedApps) {
 
-                    else {
-                        pushMessage("I could not find the program you specified", MessageType.STRING);
+                        if(!searchApp()){
+
+                            if(askFurtherSearch()){
+                                listedApps.clear();
+
+                                try {
+                                    listedApps.addAll(FileSystem.listAllLinks());
+
+                                    if(!searchApp()){
+                                        pushMessage("I am sorry, " + programName.get() + " could not be found", MessageType.STRING);
+                                    }
+
+                                }
+
+                                catch (FileSystem.UnsupportedOSException e) {
+                                    e.printStackTrace();
+                                    pushMessage("This skill is not supported on your operating system", MessageType.STRING);
+                                }
+
+                            }
+
+                        }
+
                     }
 
                 }
 
-                else{
-                    pushMessage("You did not tell me which program to open", MessageType.STRING);
+                else {
+                    pushMessage("No program specified", MessageType.STRING);
                 }
 
             }
